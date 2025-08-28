b9c57411636c06e8a4190211ab3712d03fe2dd28
Divyawankhade <divyawwawnkhade5@gmail.com>
2025-08-28 19:13:52 +0530

    chnages

---
+++ store/DummyComponent.jsx
@@ -0,0 +1,14 @@
+
+// Define the functional component
+const DummyComponent= ({ name, age, isHappy }) => {
+  return (
+    <div>
+      <h1>Hello from Dummy Component!</h1>
+      <p>My name is {name}.</p>
+      <p>I am {age} years old.</p>
+      {isHappy ? <p>I am feeling happy today!</p> : <p>I am not feeling happy today......</p>}
+    </div>
+  );
+};
+
+export default DummyComponent;
